9a7fa133610cda7ca25d23e6ee3681df7dd8bbd5
lenjedi <115083227+lenjedi@users.noreply.github.com>
2022-10-05 08:26:24 -0400

    Update fract.py

--- fract.py
+++ fract.py
@@ -1,1 +1,27 @@
-
+import turtle
+import math
+def fract(leng):
+  turtle.seth(225)
+  turtle.forward(leng)
+  if leng>3:
+    fract(leng/2)
+  turtle.seth(225)
+  turtle.backward(leng)
+  turtle.left(90)
+  turtle.forward(leng)
+  if leng>3:
+    fract(leng/2)
+  turtle.seth(315)
+  turtle.backward(leng)
+def Main():
+  leng=float(input("how big? "))
+  y=math.sqrt(2*(leng**2))
+  y=y/2
+  turtle.penup()
+  turtle.sety(y)
+  turtle.down()
+  turtle.color("black")
+  fract(leng)
+  turtle.done
+if __name__ == "__main__":
+  Main()
